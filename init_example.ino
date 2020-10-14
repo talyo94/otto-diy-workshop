fe9a43e1a906ba8b38692e55a7970468534e4ab3
Talyo <royt@nsogroup.com>
2020-10-14 18:42:29 +0300

    Added dance to init_example.ino

--- init_example.ino
+++ init_example.ino
@@ -12,12 +12,35 @@ byte buzzer = 12;
 byte trigPin = 8;
 byte echoPin = 9;
 
+int distance = 0;
+
+void startDance(); // declaration
 void setup() {
-  // put your setup code here, to run once:
-  Otto.init(leftLeg, rightLeg, leftFoot, false, 0, buzzer, trigPin, echoPin);
+ // put your setup code here, to run once:
+ Otto.init(leftLeg, rightLeg, leftFoot, false, 0, buzzer, trigPin, echoPin);
+ Otto.home(); // reset position
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ // put your main code here, to run repeatedly:
+   distance = Otto.getDistance();
+
+   if (distance < 200) {
+       startDance();
+   } else {
+       Otto.walk(3, 1000, 1);
+   }
+   delay(500);
+}
+
+void startDance() {
+   Otto.walk(3, 1000, 1);
+   delay(100);
+   Otto.moonwalker(3,800,25,1);
+   Otto.home();
+   Otto.turn(2, 800, -1);
+   Otto.delay(50);
+   Otto.jump(1, 2000);
+   Otto.home();
 
 }
